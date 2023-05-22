1093db2a94585695289e6a25218e90ca96791067
NaufalJohana <naufaljohana@gmail.com>
2023-05-22 11:44:39 +0700

    integer main menampilkan hasil

--- ParadigmaOOP_119/ParadigmaOOP_119.cpp
+++ ParadigmaOOP_119/ParadigmaOOP_119.cpp
@@ -37,4 +37,10 @@ class pelajar : public orang {
 		}
 };
 
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "hasil = " << siswa1.jumlah(10, 90) << endl;
 
+	return 0;
+}
